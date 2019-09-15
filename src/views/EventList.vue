d46b2001ca04f6b6556be948d308d72e4af94ff7
rodriguesporan <rodriguesporan@gmail.com>
2019-09-15 07:48:50 -0300

    Created dynamic routes

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <h1>Events Listing</h1>
+
+    <router-link :to="{ name: 'event-show', params: { id: 1 } }"
+      >Show event #1</router-link
+    >
   </div>
 </template>
 
--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <h1>Showing Event #1</h1>
+    <h1>Showing Event #{{ id }}</h1>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  props: ['id']
+}
 </script>
 
 <style lang="scss" scoped></style>
